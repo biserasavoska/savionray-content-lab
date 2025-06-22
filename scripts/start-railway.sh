72b9fd6ebeb7f31063d05c7fd06076587bc1eb10
Bisera Savoska <biserasavoska@gmail.com>
2025-06-22 16:56:48 +0200

    chore: add extensive logging to startup script

--- scripts/start-railway.sh
+++ scripts/start-railway.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-npx next start -p $PORT 
+
+# Log everything to see what's happening
+echo "--- Running start-railway.sh ---"
+echo "--- Checking environment variables ---"
+printenv
+echo "--- PORT from env is: [$PORT] ---"
+
+if [ -z "$PORT" ]; then
+  echo "PORT variable is not set. Defaulting to 3000 and expecting failure."
+  PORT=3000
+fi
+
+echo "--- Starting Next.js on port $PORT ---"
+npx next start -p $PORT
+echo "--- Next.js process exited ---" 
